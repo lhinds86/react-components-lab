56b209b87cf59f52e1b9cba95c25904b3913f16b
lhinds86 <leonhinds1986@gmail.com>
2024-06-11 00:47:04 -0400

    Finished HW

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,61 @@
+import './WeatherForecast.css'
+import WeatherForecast from './WeatherForecast'
 
-import './App.css'
+const App = () => {
 
-function App() {
- 
-
+  const weatherForecast = [
+    {
+      day: 'Mon',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/day.svg',
+      imgAlt: 'sun icon',
+      conditions: 'sunny',
+      time: 'Morning',
+    },
+    {
+      day: 'Tue',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg',
+      imgAlt: 'moon icon',
+      conditions: 'clear',
+      time: 'Night',
+    },
+    {
+      day: 'Wed',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg',
+      imgAlt: 'clouds with lightning icon',
+      conditions: 'stormy',
+      time: 'All Day',
+    },
+    {
+      day: 'Thu',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-day_t7ckxp.svg',
+      imgAlt: 'sun overcast by clouds icon',
+      conditions: 'overcast',
+      time: 'Evening',
+    },
+    {
+      day: 'Fri',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg',
+      imgAlt: 'moon overcast by clouds icon',
+      conditions: 'cloudy',
+      time: 'Night',
+    },
+  ]
+  console.log(weatherForecast)
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Local Weather</h1>
+      <section>
+        <WeatherForecast weather={weatherForecast[0]}/>
+        <WeatherForecast weather={weatherForecast[1]}/>
+        <WeatherForecast weather={weatherForecast[2]}/>
+        <WeatherForecast weather={weatherForecast[3]}/>
+        <WeatherForecast weather={weatherForecast[4]}/>
+        
+      </section>
+     
+      
+    </>
+
   )
 }
 
